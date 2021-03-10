ff60bd46eff69dd4e55a519ed98a4e6ae1e17e13
shaarifulz <shaarifulz@gmail.com>
2021-03-10 14:16:04 +0600

    Spring cloud Gateway project initialize, working

--- MicroserviceProduct/src/main/java/com/exam/controller/ProductController.java
+++ MicroserviceProduct/src/main/java/com/exam/controller/ProductController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.exam.model.ProductModel;
@@ -16,6 +17,7 @@ import com.exam.payload.CommonViewModel;
 import com.exam.service.IProductService;
 
 @RestController
+@RequestMapping("/product")
 public class ProductController {
 
 	@Autowired
